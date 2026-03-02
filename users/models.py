2f0db3718d97c66f31ce67980b142c03803db61b
vboxcasi <vboxcasi@UbuntuVM.myguest.virtualbox.org>
2026-03-02 13:26:08 +0100

    Added saasNotes secret file

--- users/models.py
+++ users/models.py
@@ -1,3 +1,48 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    PLAN_CHOICES = [
+        ("solo", "Solo"),
+        ("team", "Team"),
+        ("enterprise", "Enterprise"),
+    ]
+
+    TYPE_CHOICES = [
+        ("individual", "Individual"),
+        ("firm", "Firm"),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=255)
+
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE_CHOICES,
+    )
+
+    subscription_plan = models.CharField(
+        max_length=20,
+        choices=PLAN_CHOICES,
+        default="solo",
+    )
+
+    max_users = models.PositiveIntegerField(default=1)
+    max_projects = models.PositiveIntegerField(default=5)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Enforce rule: individuals can only have 1 user
+        if self.type == "individual":
+            self.max_users = 1
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
